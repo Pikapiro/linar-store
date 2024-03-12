2d1ccf82ebb4415679e72500d04bfe5ae8655727
samsystem96 <danisamsys@gmail.com>
2024-03-12 15:37:27 +0200

    cדד

--- components/preview-modal.tsx
+++ components/preview-modal.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import usePreviewModal from "@/hooks/use-preview-modal";
-import Gallery from "@/components/gallery";
 import Info from "@/components/info";
 import Modal from "@/components/ui/modal";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 
 const PreviewModal = () => {
   const previewModal = usePreviewModal();
-  const product = usePreviewModal((state) => state.data);
+  const products:string[] = ['בונדטק','אקריל','P.C uv','FR P.V.D.F','PVC FOAM']
 
-  if (!product) {
+
+  if (!products) {
     return null;
   }
 
@@ -21,10 +21,9 @@ const PreviewModal = () => {
     >
       <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
         <div className="sm:col-span-4 lg:col-span-5">
-          <Gallery images={product.images} />
         </div>
         <div className="sm:col-span-8 lg:col-span-7">
-          <Info data={product} />
+          <Info data={products} />
         </div>
       </div>
     </Modal>
